741ae1f505633d2366622a2b3e3d213250d275d0
Elizaveta Gaidarova <gaidarova.liza@gmail.com>
2022-05-03 14:20:44 +0300

    commit

--- src/componets/Converter.js
+++ src/componets/Converter.js
@@ -1,56 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import cn from "classnames";
+import { MAX_HEX_LENGTH } from "../constans";
+import { hex2rgb, validateHex } from "../utils";
+
 
 export default function Converter() {
-	const [form, setForm] = useState({
-		value: '',
-	});
+	const [color, setColor] = useState("");
 
-	const handleValueChange = evt => {
-		setForm(prevForm => ({ ...prevForm, value: evt.target.value }));
-	}
+	const handleValueChange = (evt) => {
+		setColor(evt.target.value);
+	};
 
-	const hex2rgb = h => {
-		let r = 0, g = 0, b = 0;
-		if (h.length == 7) {
-			r = "0x" + h[1] + h[2];
-			g = "0x" + h[3] + h[4];
-			b = "0x" + h[5] + h[6];
-		}
-		return "rgb(" + +r + "," + +g + "," + +b + ")";
-	}
-
-	const checkHex = hex => {
-		const hexRegex = /^[#]*([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i
-		if (hexRegex.test(hex)) {
-			return true;
-		} else {
-			return false
-		}
-	}
+	const colorIsReadyToValidate = color.length === MAX_HEX_LENGTH;
+	const colorIsValid = colorIsReadyToValidate ? validateHex(color) : true;
+	const convertedHex2Rgb = colorIsValid ? hex2rgb(color) : "";
+	const hasError = !colorIsValid;
 
 	return (
-		<form className={checkHex(form.value) === true || form.value.length < 7
-			? 'wrapper'
-			: 'wrapper active'
-		}
-			style={form.value.length === 7
-				? { backgroundColor: hex2rgb(form.value) }
-				: null
-			}
+		<form
+			className={cn("wrapper", {
+				error: hasError,
+			})}
+			style={{
+				backgroundColor: colorIsReadyToValidate ? convertedHex2Rgb : "",
+			}}
 		>
-			<div className='container'>
-				<input className='input' value={form.value} onChange={handleValueChange} />
-				<input className='input'
-					value={checkHex(form.value) === true || form.value.length < 7
-						? hex2rgb(form.value)
-						: 'Warning!'
-					}
-					style={checkHex(form.value) === true || form.value.length < 7
-						? null
-						: { backgroundColor: '#B22222', color: 'white' }
-					}
+			<div className="container">
+				<input className="input" value={color} onChange={handleValueChange} />
+				<input
+					className={cn("input", {
+						error: hasError,
+					})}
+					value={hasError ? "Warning!" : convertedHex2Rgb}
 				/>
 			</div>
-		</form >
+		</form>
 	);
 }
